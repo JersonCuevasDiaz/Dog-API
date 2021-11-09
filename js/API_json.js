c489502b1552420fd0b2ebd51f0dbf2358be0900
jersoncuevas <thejer19@gmail.com>
2021-11-09 16:50:41 -0400

    optimize files

--- js/API_json.js
+++ js/API_json.js
@@ -1,25 +1,19 @@
-const d = document;
-const img_dog = d.getElementById("image_dog");
-const btn1 = d.getElementById("btn2");
+import{doc,img_dog,btn2} from './main.js';
 
+doc.addEventListener("click", (e)=>{
+    e.preventDefault();
+    if (e.target.matches("#btn2")){
+        let rand = 'random';
+        let dogs = `https://dog.ceo/api/breeds/image/${rand}`;
 
-d.addEventListener("click", (e)=>{
-    e.preventDefault()
-    if(e.target.matches("#btn2")){
-        let c = ['random']
-        let dogs = `https://dog.ceo/api/breeds/image/${c}`;
-    
-    
         fetch(dogs)
-        .then(resp => resp.json())
-        .then(inf =>{
-            console.log(inf.message)
-            console.log(img_dog)
-            img_dog.innerHTML = `<img src= ${inf.message}>`
-        }) 
-    }else{return false}
-    
-})
+            .then(resp => resp.json())
+            .then(inf => {
+                img_dog.innerHTML = `<img src= ${inf.message}>`;
+            });
+    } else { return false; }
+
+});
 
 
 
---
+++ js/main.js
@@ -0,0 +1,7 @@
+const doc = document;
+const img_dog = doc.getElementById("image_dog");
+const btn2 = doc.getElementById("btn2");
+
+export{doc,img_dog,btn2}
+
+
